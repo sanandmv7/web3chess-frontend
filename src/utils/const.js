692648e646cf4b2c74f8f4e8582343ea4335386b
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2024-04-08 14:54:37 +0530

    Update websocket urls

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,3 +1,3 @@
-export const webSocketURL = "ws://127.0.0.1:4000";
-export const chatSocketURL = "ws://127.0.0.1:4001";
+export const webSocketURL = "ws://web3chess-backend.onrender.com:4000";
+export const chatSocketURL = "ws://web3chess-chatserver.onrender.com:4001";
 
