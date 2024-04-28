994d5d5f8df4a6a9411aaac2f12975402ead1512
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2024-04-28 22:59:33 +0530

    update consts

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,4 @@
-// export const webSocketURL = "wss://web3chess-backend.onrender.com";
-// export const chatSocketURL = "wss://web3chess-chatserver.onrender.com";
-export const webSocketURL = "ws://127.0.0.1:4000";
-export const chatSocketURL = "ws://127.0.0.1:4001";
+export const webSocketURL = "wss://web3chess-backend.onrender.com";
+export const chatSocketURL = "wss://web3chess-chatserver.onrender.com";
+// export const webSocketURL = "ws://127.0.0.1:4000";
+// export const chatSocketURL = "ws://127.0.0.1:4001";
